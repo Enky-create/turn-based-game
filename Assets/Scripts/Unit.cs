916af5bdfef0fcb2fedd6e1cd9ad8f255a8fa82e
Enky-create <komkowvadim@gmail.com>
2026-01-06 23:52:45 +0200

    Animation of a unit

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     private Vector3 targetPosition;
-    [SerializeField]private int speed = 7;
-    
+    [SerializeField] private int speed = 7;
+    [SerializeField] private Animator animator;
+    [SerializeField] private int rotationSpeed = 10;
     private void Move( Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
@@ -28,6 +29,12 @@ public class Unit : MonoBehaviour
         {
             Vector3 moveDirection = (targetPosition - this.transform.position).normalized;
             this.transform.position += moveDirection * speed * Time.deltaTime;
+            animator.SetBool("IsWalking", true);
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection,Time.deltaTime*rotationSpeed);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
         }
     }
 }
