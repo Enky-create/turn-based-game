86d66da2a6749df60ff756fc853f09fc6a346501
Enky-create <komkowvadim@gmail.com>
2026-01-21 21:58:48 +0200

    better camera control

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,9 +1,15 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    private const float MIN_FOLLOW_OFFSET_Y = 2f;
+    private const float MAX_FOLLOW_OFFSET_Y = 12f;
+    [SerializeField] private CinemachineFollow cinemachineFollow;
     [SerializeField] private int moveSpeed;
     [SerializeField] private int rotationSpeed;
+    [SerializeField] private float zoomAmount;
     [SerializeField]private LayerMask layerMask;
     void Start()
     {
@@ -19,15 +25,18 @@ public class CameraControl : MonoBehaviour
         if (Input.GetKey(KeyCode.S)) moveVector.z -= 1;
         if (Input.GetKey(KeyCode.A)) moveVector.x -= 1;
         if (Input.GetKey(KeyCode.D)) moveVector.x += 1;
-        transform.position += moveVector.normalized * moveSpeed * Time.deltaTime;
+        transform.position += (transform.right* moveVector.x + transform.forward * moveVector.z) * moveSpeed * Time.deltaTime;
         //Rotation
-        
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, layerMask))
+        var rotationVector = new Vector3(0,0,0);
+        if(Input.GetKey(KeyCode.E)) rotationVector.y += 1;
+        if (Input.GetKey(KeyCode.Q)) rotationVector.y -= 1;
+        transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+        var cinimachineOffset = cinemachineFollow.FollowOffset;
+        if (Input.mouseScrollDelta.y != 0)
         {
-            var rotationVector = (hitInfo.point-transform.position).normalized;
-            transform.forward = Vector3.Lerp(transform.forward, rotationVector, rotationSpeed * Time.deltaTime);
+            cinimachineOffset.y += Input.mouseScrollDelta.y * zoomAmount;
         }
-        
+        cinimachineOffset.y = Mathf.Clamp(cinimachineOffset.y, MIN_FOLLOW_OFFSET_Y, MAX_FOLLOW_OFFSET_Y);
+        cinemachineFollow.FollowOffset = Vector3.Lerp(cinemachineFollow.FollowOffset, cinimachineOffset, 10);
     }
 }
