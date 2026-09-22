6ffeb7534133bec650009c2e72771991701083da
Enky-create <komkowvadim@gmail.com>
2026-09-22 23:44:48 +0700

    grenade visuals

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -1,27 +1,43 @@
 using System;
+using System.Runtime.Serialization;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
+    public static EventHandler ON_ANY_GRENADE_EXPLOTION;
     private Action onActionComplete;
     private Vector3 targetPosition;
     private float minimumExplotionDistance=.2f;
+    private float distance;
     [SerializeField] private float explotionRadius=7f;
     [SerializeField] private int speed = 15;
     [SerializeField] private int damage = 15;
+    [SerializeField] private Transform grenadeVFX;
+    [SerializeField] private AnimationCurve animationCurve;
+    private Vector3 positionXZ;
+
     public void Setup(Vector3 targetPosition, Action onActionComplete)
     {
         this.targetPosition = targetPosition;
         this.onActionComplete = onActionComplete;
+        
+        positionXZ=transform.position;
+        positionXZ.y=0;
+        distance = Vector3.Distance(positionXZ, targetPosition);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        var moveDirection = (targetPosition - transform.position).normalized;
-        transform.position+= moveDirection * speed * Time.deltaTime;
-        if(Vector3.Distance(transform.position, targetPosition) < minimumExplotionDistance)
+        var currentDistance= Vector3.Distance(positionXZ, targetPosition);
+        var moveDirection = (targetPosition-positionXZ).normalized;
+
+        positionXZ +=  moveDirection*speed*Time.deltaTime;
+        var Yposition = distance/4f * animationCurve.Evaluate(1-currentDistance/distance);
+        transform.position =  new Vector3(positionXZ.x,Yposition,positionXZ.z);
+        if(Vector3.Distance(positionXZ, targetPosition) < minimumExplotionDistance)
         {
             Collider [] colliders = Physics.OverlapSphere(transform.position,explotionRadius);
             foreach( Collider collider in colliders){
@@ -31,6 +47,8 @@ public class Grenade : MonoBehaviour
                 }
             }
             onActionComplete();
+            Instantiate(grenadeVFX,transform.position,Quaternion.identity);
+            ON_ANY_GRENADE_EXPLOTION?.Invoke(this,EventArgs.Empty);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/ScreenShakeActions.cs
+++ Assets/Scripts/ScreenShakeActions.cs
@@ -7,6 +7,12 @@ public class ScreenShakeActions : MonoBehaviour
     void Start()
     {
         ShootAction.OnAnyShoot+=ShootAction_OnAnyShoot;
+        Grenade.ON_ANY_GRENADE_EXPLOTION+=Grenade_ON_ANY_GRENADE_EXPLOTION;
+    }
+
+    private void Grenade_ON_ANY_GRENADE_EXPLOTION(object sender, EventArgs e)
+    {
+        ScreenShake.Instance.Shake(15);
     }
 
     private void ShootAction_OnAnyShoot(object sender, ShootAction.ShootEventArgs e)
